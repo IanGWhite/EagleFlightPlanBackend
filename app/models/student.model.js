f03d177088336560fbbd04e4d2a71705529ff3ed
Samantha Wiggs <Samantha.leigh.wiggs@gmail.com>
2025-02-28 13:56:57 -0600

    fixed points to be integers

--- app/models/student.model.js
+++ app/models/student.model.js
@@ -13,7 +13,7 @@ module.exports = (sequelize, Sequelize) => {
       type: Sequelize.STRING,
     },
     points: {
-      type: Sequelize.STRING,
+      type: Sequelize.INTEGER,
     },
     semestersFromGrad: {
       type: Sequelize.STRING,
