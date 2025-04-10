6f08b95825ac37f527ebf77e83d7cad661ead1e5
IanGWhite <97561180+IanGWhite@users.noreply.github.com>
2025-04-10 16:04:57 -0500

    added pointlog routes

---
+++ app/routes/pointLog.routes.js
@@ -0,0 +1,31 @@
+module.exports = (app) => {
+    const pointLogs = require("../controllers/pointLog.controller.js");
+    const { authenticate } = require("../authorization/authorization.js");
+    var router = require("express").Router();
+  
+    // Create a new PointLog for a Student
+    //router.post("/:studentId/pointLogs/", [authenticate], pointLogs.create);
+    router.post("/:studentId/pointLogs/", pointLogs.create);
+  
+    // Retrieve all PointLogs for a Student
+    router.get(
+      "/:studentId/pointLogs/",
+      pointLogs.findAllForStudent
+    );
+  
+    // Retrieve a single PointLog with id
+    //router.get("/:studentId/pointLogs/:id", [authenticate], pointLogs.findOne);
+    router.get("/:studentId/pointLogs/:id", pointLogs.findOne);
+  
+    // Update a PointLog with id
+    router.put("/:studentId/pointLogs/:id", pointLogs.update);
+  
+    // Delete a PointLog with id
+    router.delete("/:studentId/pointLogs/:id", pointLogs.delete);
+  
+    // Delete all PointLogs
+    router.delete("/:studentId/pointLogs/deleteAll", pointLogs.deleteAll);
+  
+    app.use("/flight-plan-t5/students", router);
+  };
+  
