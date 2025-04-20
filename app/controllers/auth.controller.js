0ac235e44bd7eba4342528d517b6abb708dcfd56
Samantha Wiggs <Samantha.leigh.wiggs@gmail.com>
2025-04-20 16:09:44 -0500

    login in issue fixed

--- app/controllers/auth.controller.js
+++ app/controllers/auth.controller.js
@@ -7,21 +7,16 @@ const StudentMajor = db.studentMajors;
 const StudentStrengths = db.studentStrengths;
 const Op = db.Sequelize.Op;
 const UserRole = db.userRole;
-
-
 const { google } = require("googleapis");
 
 var jwt = require("jsonwebtoken");
 
 let googleUser = {};
-
 const google_id = process.env.CLIENT_ID;
 
 exports.login = async (req, res) => {
   console.log(req.body);
-
   var googleToken = req.body.credential;
-
   const { OAuth2Client } = require("google-auth-library");
   const client = new OAuth2Client(google_id);
   async function verify() {
@@ -32,251 +27,108 @@ exports.login = async (req, res) => {
     googleUser = ticket.getPayload();
     console.log("Google payload is " + JSON.stringify(googleUser));
   }
-  await verify().catch(console.error);
+  try {
+    // Verify the Google token
+    await verify().catch(console.error);
+    let email = googleUser.email;
+    let firstName = googleUser.given_name;
+    let lastName = googleUser.family_name;
 
-  let email = googleUser.email;
-  let firstName = googleUser.given_name;
-  let lastName = googleUser.family_name;
-
-  // if we don't have their email or name, we need to make another request
-  // this is solely for testing purposes
-  if (
-    (email === undefined ||
-      firstName === undefined ||
-      lastName === undefined) &&
-    req.body.accessToken !== undefined
-  ) {
-    let oauth2Client = new OAuth2Client(google_id); // create new auth client
-    oauth2Client.setCredentials({ access_token: req.body.accessToken }); // use the new auth client with the access_token
-    let oauth2 = google.oauth2({
-      auth: oauth2Client,
-      version: "v2",
-    });
-    let { data } = await oauth2.userinfo.get(); // get user info
-    console.log(data);
-    email = data.email;
-    firstName = data.given_name;
-    lastName = data.family_name;
-  }
-
-  console.log(lastName);
-
-  let user = {};
-  let session = {};
-  let student = {};
-  let userRole = {};
-  let studentMajor = {};
-  let studentStrengths = {};
-
-  await User.findOne({
-    where: {
-      email: email,
-    },
-  })
-    .then((data) => {
-      if (data != null) {
-        user = data.dataValues;
-      } else {
-        // create a new User and save to database
-        user = {
-          email: email,
-          admin: 0,
-          fName: firstName,
-          lName: lastName,
-          isStudent: true
-        };
-      }
-    })
-    .catch((err) => {
-      res.status(500).send({ message: err.message });
-    });
-
-  // this lets us get the user id
-  if (user.id === undefined) {
-    console.log("need to get user's id");
-    console.log(user);
-    await User.create(user)
-      .then((data) => {
-        console.log("user was registered");
-        user = data.dataValues;
-        // res.send({ message: "User was registered successfully!" });
-      })
-      .catch((err) => {
-        res.status(500).send({ message: err.message });
-      });
-  } else {
-    console.log(user);
-    // doing this to ensure that the user's name is the one listed with Google
-    console.log(user);
-    await User.update(user, { where: { id: user.id } })
-      .then((num) => {
-        if (num == 1) {
-          console.log("updated user's name");
-        } else {
-          console.log(
-            `Cannot update User with id=${user.id}. Maybe User was not found or req.body is empty!`
-          );
-        }
-      })
-      .catch((err) => {
-        console.log("Error updating User with id=" + user.id + " " + err);
-      });
-  }
-
-  //trying to find the student
-  await Student.findOne({
-    where: {
-      userId: user.id,
-      fName: firstName,
-      lName: lastName,
-      studentId: user.id,
-    },
-  })
-    .then((data) => {
-      if (data != null) {
-        student = data.dataValues;
-      } else {
-        // create a new Student and save to database
-        student = {
-          userId: user.id,
-          fName: firstName,
-          lName: lastName,
-          studentId: user.id,
-          points: 0,
-          semestersFromGrad: 8,
-          estimatedGradSemester: 8,
-          studentIdNo: 0,
-
-        };
-      }
-    })
-    .catch((err) => {
-      res.status(500).send({ message: err.message });
-    });
-
-  //create student
-  if (student.id === undefined) {
-    console.log("need to get student's id");
-    console.log(student);
-    await Student.create(student)
-      .then((data) => {
-        console.log("student was registered");
-        student = data.dataValues;
-        // res.send({ message: "student was registered successfully!" });
-      })
-      .catch((err) => {
-        res.status(500).send({ message: err.message });
-      });
-  } else {
-    console.log(student);
-    // doing this to ensure that the student's name is the one listed with Google
-    student.userId = user.id;
-    student.fName = firstName;
-    student.lName = lastName;
-    console.log(student);
-    await Student.update(student, { where: { id: student.id } })
-      .then((num) => {
-        if (num == 1) {
-          console.log("updated student's name");
-        } else {
-          console.log(
-            `Cannot update Student with id=${student.id}. Maybe User was not found or req.body is empty!`
-          );
-        }
-      })
-      .catch((err) => {
-        console.log("Error updating Student with id=" + student.id + " " + err);
-      });
+    if (!email || !firstName || !lastName) {
+      // handle missing data or create another request logic here
     }
-        //trying to find the userRole
-    await UserRole.findOne({
-      where: {
-        userId: user.id,
-      },
-    })
+
+    // Find or create User
+    let user = {};
+    await User.findOne({ where: { email: email } })
       .then((data) => {
         if (data != null) {
-          userRole = data.dataValues;
+          user = data.dataValues;
         } else {
-          // create a new Student and save to database
-          userRole = {
+          user = { email: email, admin: 0, fName: firstName, lName: lastName, isStudent: true };
+        }
+      });
+
+    // Check if user id is found before proceeding
+    if (!user.id) {
+      await User.create(user).then((data) => {
+        user = data.dataValues;
+      });
+    }
+
+    // Find or create Student
+    let student = {};
+    await Student.findOne({ where: { userId: user.id, fName: firstName, lName: lastName, studentId: user.id } })
+      .then((data) => {
+        if (data != null) {
+          student = data.dataValues;
+        } else {
+          student = {
             userId: user.id,
-            roleId : 1
+            fName: firstName,
+            lName: lastName,
+            studentId: user.id,
+            points: 0,
+            semestersFromGrad: 8,
+            estimatedGradSemester: 8,
+            studentIdNo: 0,
           };
         }
-      })
-      .catch((err) => {
-        res.status(500).send({ message: err.message });
       });
 
-    //create userRole
-    if (UserRole.id === undefined) {
-      console.log("need to get userRole's id");
-      console.log(UserRole);
-      await UserRole.create(userRole)
-        .then((data) => {
-          console.log("userRole was registered");
-          userRole = data.dataValues;
-          // res.send({ message: "userRole was registered successfully!" });
-        })
-        .catch((err) => {
-          res.status(500).send({ message: err.message });
-        });
+    // If student doesn't exist, create one
+    if (!student.id) {
+      await Student.create(student).then((data) => {
+        student = data.dataValues;
+      });
     } else {
-      console.log(userRole);
-      // doing this to ensure that the userRole's name is the one listed with Google
-      console.log(userRole);
+      student.userId = user.id;
+      student.fName = firstName;
+      student.lName = lastName;
+      await Student.update(student, { where: { id: student.id } });
     }
 
-    //trying to find the StudentMajor
-    await StudentMajor.findOne({
-      where: {
-        studentId: student.id,
-      },
-    })
+    // Find or create UserRole
+    let userRole = {};
+    await UserRole.findOne({ where: { userId: user.id } })
+      .then((data) => {
+        if (data != null) {
+          userRole = data.dataValues;
+        } else {
+          userRole = { userId: user.id, roleId: 1 };
+        }
+      });
+
+    // If userRole doesn't exist, create one
+    if (!userRole.id) {
+      await UserRole.create(userRole).then((data) => {
+        userRole = data.dataValues;
+      });
+    }
+
+    // Find or create StudentMajor
+    let studentMajor = {};
+    await StudentMajor.findOne({ where: { studentId: student.id } })
       .then((data) => {
         if (data != null) {
           studentMajor = data.dataValues;
         } else {
-          // create a new Student and save to database
-          studentMajor = {
-            studentId: student.id,
-            majorId : 1
-          };
+          studentMajor = { studentId: student.id, majorId: 1 };
         }
-      })
-      .catch((err) => {
-        res.status(500).send({ message: err.message });
       });
 
-
-    //create StudentMajor
-    if (StudentMajor.id === undefined) {
-      console.log("need to get userRole's id");
-      console.log(StudentMajor);
-      await StudentMajor.create(studentMajor)
-        .then((data) => {
-          console.log("userRole was registered");
-          studentMajor = data.dataValues;
-          // res.send({ message: "userRole was registered successfully!" });
-        })
-        .catch((err) => {
-          res.status(500).send({ message: err.message });
-        });
-    } else {
-      console.log(studentMajor);
-      // doing this to ensure that the userRole's name is the one listed with Google
-      console.log(studentMajor);
+    // If StudentMajor doesn't exist, create one
+    if (!studentMajor.id) {
+      await StudentMajor.create(studentMajor).then((data) => {
+        studentMajor = data.dataValues;
+      });
     }
 
-    const existingStrengths = await StudentStrengths.findAll({
-      where: { studentId: student.id }
-    });
-    
-    // If not exactly 5, delete and recreate
+    // Handle StudentStrengths - Ensure exactly 5 strengths
+    const existingStrengths = await StudentStrengths.findAll({ where: { studentId: student.id } });
     if (!existingStrengths || existingStrengths.length !== 5) {
       await StudentStrengths.destroy({ where: { studentId: student.id } });
-    
+
       const newStrengths = [
         { studentId: student.id, strengthId: 1 },
         { studentId: student.id, strengthId: 1 },
@@ -284,145 +136,72 @@ exports.login = async (req, res) => {
         { studentId: student.id, strengthId: 1 },
         { studentId: student.id, strengthId: 1 },
       ];
-    
+
       for (const strength of newStrengths) {
         await StudentStrengths.create(strength);
       }
-    
       console.log("StudentStrengths reset with 5 strengths");
-    } else {
-      console.log("Student already has 5 strengths");
     }
-    
 
-  // try to find session first
-
-  await Session.findOne({
-    where: {
-      email: email,
-      token: { [Op.ne]: "" },
-    },
-  })
-    .then(async (data) => {
-      if (data !== null) {
-        session = data.dataValues;
-        if (session.expirationDate < Date.now()) {
-          session.token = "";
-          // clear session's token if it's expired
-          await Session.update(session, { where: { id: session.id } })
-            .then((num) => {
-              if (num == 1) {
-                console.log("successfully logged out");
-              } else {
-                console.log("failed");
-                res.send({
-                  message: `Error logging out user.`,
-                });
-              }
-            })
-            .catch((err) => {
-              console.log(err);
-              res.status(500).send({
-                message: "Error logging out user.",
-              });
-            });
-          //reset session to be null since we need to make another one
-          session = {};
-        } else {
-          // if the session is still valid, then send info to the front end
-          let userInfo = {
-            email: user.email,
-            fName: firstName,
-            lName: lastName,
-            userId: user.id,
-            studentId: student.id,
-            token: session.token,
-            // refresh_token: user.refresh_token,
-            // expiration_date: user.expiration_date
-          };
-          console.log("found a session, don't need to make another one");
-          console.log(userInfo);
-          res.send(userInfo);
+    // Find existing session or create a new one
+    let session = {};
+    await Session.findOne({ where: { email: email, token: { [Op.ne]: "" } } })
+      .then(async (data) => {
+        if (data) {
+          session = data.dataValues;
+          if (session.expirationDate < Date.now()) {
+            session.token = "";
+            await Session.update(session, { where: { id: session.id } });
+            session = {};  // reset session for new one
+          } else {
+            let userInfo = { email: user.email, fName: firstName, lName: lastName, userId: user.id, studentId: student.id, token: session.token };
+            return res.send(userInfo);  // Send response here and stop
+          }
         }
-      }
-    })
-    .catch((err) => {
-      res.status(500).send({
-        message:
-          err.message || "Some error occurred while retrieving sessions.",
-      });
-    });
-
-  if (session.id === undefined) {
-    // create a new Session with an expiration date and save to database
-    let token = jwt.sign({ id: email }, authconfig.secret, {
-      expiresIn: 86400,
-    });
-    let tempExpirationDate = new Date();
-    tempExpirationDate.setDate(tempExpirationDate.getDate() + 1);
-    const session = {
-      token: token,
-      email: email,
-      userId: user.id,
-      expirationDate: tempExpirationDate,
-    };
-
-    console.log("making a new session");
-    console.log(session);
-
-    await Session.create(session)
-      .then(() => {
-        let userInfo = {
-          email: user.email,
-          fName: firstName,
-          lName: lastName,
-          userId: user.id,
-          studentId: student.id,
-          token: token,
-          // refresh_token: user.refresh_token,
-          // expiration_date: user.expiration_date
-        };
-        console.log(userInfo);
-        res.send(userInfo);
       })
       .catch((err) => {
-        res.status(500).send({ message: err.message });
+        return res.status(500).send({ message: err.message });
       });
+
+    // Create a new session if no valid session found
+    if (!session.id) {
+      let token = jwt.sign({ id: email }, authconfig.secret, { expiresIn: 86400 });
+      let tempExpirationDate = new Date();
+      tempExpirationDate.setDate(tempExpirationDate.getDate() + 1);
+
+      await Session.create({ token, email, userId: user.id, expirationDate: tempExpirationDate })
+        .then(() => {
+          let userInfo = { email: user.email, fName: firstName, lName: lastName, userId: user.id, studentId: student.id, token };
+          return res.send(userInfo);  // Send response here and stop
+        });
+    }
+  } catch (error) {
+    console.log(error);
+    return res.status(500).send({ message: "An error occurred during login." });
   }
 };
 
 exports.authorize = async (req, res) => {
   console.log("authorize client");
-  const oauth2Client = new google.auth.OAuth2(
-    process.env.CLIENT_ID,
-    process.env.CLIENT_SECRET,
-    "postmessage"
-  );
+  const oauth2Client = new google.auth.OAuth2(process.env.CLIENT_ID, process.env.CLIENT_SECRET, "postmessage");
 
   console.log("authorize token");
-  // Get access and refresh tokens (if access_type is offline)
   let { tokens } = await oauth2Client.getToken(req.body.code);
   oauth2Client.setCredentials(tokens);
 
   let user = {};
   console.log("findUser");
 
-  await User.findOne({
-    where: {
-      id: req.params.id,
-    },
-  })
+  await User.findOne({ where: { id: req.params.id } })
     .then((data) => {
       if (data != null) {
         user = data.dataValues;
       }
     })
     .catch((err) => {
-      res.status(500).send({ message: err.message });
-      return;
+      return res.status(500).send({ message: err.message });
     });
-  console.log("user");
-  console.log(user);
+
   user.refresh_token = tokens.refresh_token;
   let tempExpirationDate = new Date();
   tempExpirationDate.setDate(tempExpirationDate.getDate() + 100);
@@ -433,19 +212,14 @@ exports.authorize = async (req, res) => {
       if (num == 1) {
         console.log("updated user's google token stuff");
       } else {
-        console.log(
-          `Cannot update User with id=${user.id}. Maybe User was not found or req.body is empty!`
-        );
+        console.log(`Cannot update User with id=${user.id}. Maybe User was not found or req.body is empty!`);
       }
-      let userInfo = {
-        refresh_token: user.refresh_token,
-        expiration_date: user.expiration_date,
-      };
+      let userInfo = { refresh_token: user.refresh_token, expiration_date: user.expiration_date };
       console.log(userInfo);
       res.send(userInfo);
     })
     .catch((err) => {
-      res.status(500).send({ message: err.message });
+      return res.status(500).send({ message: err.message });
     });
 
   console.log(tokens);
@@ -455,55 +229,38 @@ exports.authorize = async (req, res) => {
 exports.logout = async (req, res) => {
   console.log(req.body);
   if (req.body === null) {
-    res.send({
-      message: "User has already been successfully logged out!",
-    });
+    res.send({ message: "User has already been successfully logged out!" });
     return;
   }
 
-  // invalidate session -- delete token out of session table
   let session = {};
-
   await Session.findAll({ where: { token: req.body.token } })
     .then((data) => {
       if (data[0] !== undefined) session = data[0].dataValues;
     })
     .catch((err) => {
-      res.status(500).send({
-        message:
-          err.message || "Some error occurred while retrieving sessions.",
-      });
-      return;
+      return res.status(500).send({ message: err.message });
     });
 
   session.token = "";
 
-  // session won't be null but the id will if no session was found
   if (session.id !== undefined) {
     Session.update(session, { where: { id: session.id } })
       .then((num) => {
         if (num == 1) {
           console.log("successfully logged out");
-          res.send({
-            message: "User has been successfully logged out!",
-          });
+          res.send({ message: "User has been successfully logged out!" });
         } else {
           console.log("failed");
-          res.send({
-            message: `Error logging out user.`,
-          });
+          res.send({ message: `Error logging out user.` });
         }
       })
       .catch((err) => {
         console.log(err);
-        res.status(500).send({
-          message: "Error logging out user.",
-        });
+        return res.status(500).send({ message: "Error logging out user." });
       });
   } else {
     console.log("already logged out");
-    res.send({
-      message: "User has already been successfully logged out!",
-    });
+    return res.send({ message: "User has already been successfully logged out!" });
   }
 };
